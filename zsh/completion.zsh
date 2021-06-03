137df8911ee28d53b2c08eef101982ffeb957033
Karam Fahad <5566383+karamfd@users.noreply.github.com>
2021-06-03 17:31:48 -0400

    file location: ~/.zsh/completion.zsh

---
+++ zsh/completion.zsh
@@ -0,0 +1,5 @@
+# case insensitive path-completion
+zstyle ':completion:*' matcher-list 'm:{[:lower:][:upper:]}={[:upper:][:lower:]}' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} l:|=* r:|=*' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} l:|=* r:|=*' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} l:|=* r:|=*'
+
+# partial completion suggestions
+zstyle ':completion:*' list-suffixeszstyle ':completion:*' expand prefix suffix
